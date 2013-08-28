0e090b2c481d967bbc2c85b4fa261e2045864b25
chappyhome <sgqjpw@gmail.com>
2013-08-28 09:34:56 +0800

    delete node readeram branch

--- api/constant.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$base_url       = 'http://localhost:8080';
-	$categories_url = $base_url.'/ajax/categories';
-	$book_list_url  = $base_url.'/xml';
--- api/get_subcategory_info.php
+++
@@ -1,3 +0,0 @@
-<?php
-	require_once "constant.php";
-	echo  file_get_contents($base_url);
