6ff094420a84ff44dd4c6c4cc37961b83639fc27
chappyhome <sgqjpw@gmail.com>
2013-08-28 10:14:26 +0800

    改名importBookToRedis.js  to  importBookPageToRedis.js

--- importBookPageToRedis.js
+++ importBookPageToRedis.js
@@ -4,16 +4,23 @@
      http = require("http");
 
  var PAGE_BOOK_NUMBER = 10,
- 	 PAGE_PREFIX = 'calibre_page_',
- 	 BASE_DATA_URL = 'http://www.deliverkindle.com:8080/xml';
+ 	   PAGE_PREFIX = 'calibre_page_',
+     PAGE_BASIC_INFO = 'calibre_page_basic_info',
+ 	   BASE_DATA_URL = 'http://www.deliverkindle.com:8080/xml';
 
     http.get(BASE_DATA_URL + "?start=0&num=1", function(res) {
 	    res.setEncoding('utf8');
 	    res.on('data', function(data) {
 	        parseString(data, function (err, result) {
-	        	var total = result.library.$.total,
+	        	var total = parseInt(result.library.$.total),
 	        	    pages = Math.ceil(total/PAGE_BOOK_NUMBER);
         		url = BASE_DATA_URL + '?start=0' + "&num=" + total;
+                var basic_info = {"total":total,
+                                  "pages":pages,
+                                  "page_prefix":PAGE_PREFIX,
+                                  "num":PAGE_BOOK_NUMBER};
+                client.set(PAGE_BASIC_INFO, JSON.stringify(basic_info));
+
         			//console.log(key);
     			http.get(url, function(res) {
     				res.setEncoding('utf8');
